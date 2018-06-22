e5caaefc89c1634e48dcbfd9f9597120e762a32d
Felix <felixrajesh.s@mookambikainfo.com>
2018-06-22 19:23:24 +0530

    first commits

---
+++ admin/js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+var app = angular.module("App", [
+    'ui.router',
+    'ngRoute'
+])
+.run(function($rootScope) {
+    $rootScope.api_root_url = 'http://localhost/project/ext/core/public';
+});
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/app/dashboard/main.html"
+    })
+    .when("/maincategory", {
+        templateUrl : "views/app/dashboard/maincategory.html",
+        controller : "maincategoryCtrl"
+    })
+    .when("/green", {
+        templateUrl : "views/app/dashboard/dashboard.html",
+        controller : "parisCtrl"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
+
+
+
+app.controller("parisCtrl", function ($scope) {
+    $scope.msg = "I love Paris";
+});
---
+++ admin/views/controllers/maincategoryController.js
@@ -0,0 +1,88 @@
+
+app.factory("services", ['$http', '$rootScope', function ($http, $rootScope) {
+//  var serviceBase = 'services/';
+        var serviceBase = $rootScope.api_root_url;
+        var obj = {};
+
+        obj.commonService = function (url, data) {
+            return $http.post(serviceBase + url, data).then(function (result) {
+                return result.data;
+            });
+        };
+
+        obj.getCustomers = function () {
+            return $http.get(serviceBase + 'customers');
+        }
+        obj.getCustomer = function (customerID) {
+            return $http.get(serviceBase + 'customer?id=' + customerID);
+        }
+
+        obj.insertCustomer = function (customer) {
+            return $http.post(serviceBase + 'insertCustomer', customer).then(function (results) {
+                return results;
+            });
+        };
+
+        obj.updateCustomer = function (id, customer) {
+            return $http.post(serviceBase + 'updateCustomer', {id: id, customer: customer}).then(function (status) {
+                return status.data;
+            });
+        };
+
+        obj.deleteCustomer = function (id) {
+            return $http.delete(serviceBase + 'deleteCustomer?id=' + id).then(function (status) {
+                return status.data;
+            });
+        };
+
+        return obj;
+    }]);
+
+app.controller("maincategoryCtrl", function ($scope, services, $rootScope, $http) {
+
+    $scope.formdata = {};
+    $scope.categorylist = {};
+    var result = {};
+
+    $scope.addcatlist = function (action) {
+        $scope.formdata.action = action;
+        services.commonService('/product/getcategorymaster', $scope.formdata);
+        $scope.formdata = {};
+        $scope.getcatlist('list', '');
+    }
+
+    $scope.getcatlist = function () {
+        $scope.formdata.action = 'add';
+        var result = services.commonService('/product/getcategorymaster', $scope.formdata)
+    }
+
+    $scope.getcatlist = function (action, id) {
+
+        if (id > 0) {
+            $scope.formdata.id = id;
+        }
+
+        $scope.formdata.action = action;
+        var data = $scope.formdata
+        $scope.formdata.action = 'list';
+        var apiurl = $rootScope.api_root_url + '/product/getcategorymaster';
+        $http({
+            method: "POST",
+            url: apiurl,
+            data: data
+        }).then(function mySuccess(response) {
+            if (id > 0) {
+                $scope.formdata = response.data.response.categorydetails;
+            } else {
+                $scope.categorylist = response.data.response.categorydetails;
+            }
+
+        });
+
+
+    }
+
+
+    $scope.getcatlist('list', '');
+
+});
